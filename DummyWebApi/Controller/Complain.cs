1fd8024d3a948365f1a3e7f73bc7cfce9dcfa2a3
Harshad Mehta <harshadmehta001@gmail.com>
2018-04-07 19:10:52 +0530

    anand's change merged

--- DummyWebApi/Controller/Complain.cs
+++ DummyWebApi/Controller/Complain.cs
@@ -1,5 +1,6 @@
 ﻿using BuildIndia.Service.Repository;
 using BuildIndia.ViewModel;
+using System.Collections.Generic;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -11,18 +12,36 @@ namespace DummyWebApi.Controller
     public class ComplainController : ApiController
     {
         private ComplainRepository _compRepo;
+        private LocationRepository _locReop;
+        private CompactorRepository _comprepo;
+        private class AllResult
+        {
+            public List<ComplainViewModel> complaints;
+            public List<LocationViewModel> locations;
+            public List<CompactorViewModel> compactors;
+        }
+        
        public ComplainController()
         {
             _compRepo = new ComplainRepository();
+            _locReop = new LocationRepository();
+            _comprepo = new CompactorRepository();
         }
         public IHttpActionResult Get()
         {
-            return Ok(_compRepo.GetAllComplains());
+            AllResult result = new AllResult
+            {
+                complaints = _compRepo.GetAllComplains(),
+                locations = _locReop.GetAllLocations(),
+                compactors = _comprepo.GetAllCompactors()
+            };
+            return Ok(result);
 
         }
         [HttpPost]
         public IHttpActionResult PostAddVehicle(ComplainViewModel complain)
         {
+
             _compRepo.Save(complain);
             return Ok();
         }
