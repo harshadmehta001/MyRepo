61b4011fb319d92507288f1d98f1993256b2d153
harshadmehta001 <35057276+harshadmehta001@users.noreply.github.com>
2018-04-07 11:19:17 +0530

    Add files via upload

---
+++ IComplainRepository.cs
@@ -0,0 +1,17 @@
+﻿using BuildIndia.Data;
+using BuildIndia.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BuildIndia.Service.Repository
+{
+ 
+    interface IComplainRepository
+    {
+        void Save(ComplainViewModel complain);
+        List<ComplainViewModel> GetAllComplains();
+        ComplainViewModel GetComplains(string contactNumber);
+    }
+}
---
+++ VehicleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildIndia.ViewModel
+{
+    public class VehicleViewModel
+    {
+        public string VehicleNumber { get; set; }
+        public string RegistrationYear { get; set; }
+        public string VehicleDetails { get; set; }
+        public string ServiceProvider { get; set; }
+    }
+
+    public class ComplainViewModel
+    {
+        public Int32 ComplainID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string Location { get; set; }
+
+        public string Compactor { get; set; }
+
+        public string Decription { get; set; }
+    }
+}
